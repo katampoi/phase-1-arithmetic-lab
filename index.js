ea529c919e0559ebd727b0a7b8fe1777816d3ace
katampoi <simonkatampoi@student.moringaschool.com>
2022-06-09 23:25:12 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+const multiply = num1*num2;
+const random = Math.floor(Math.random())+1
+let num3 = 14;
+let num4 = 5;
+const mod = num3 % num4;
+const max= Math.max(11,2,9,20);
